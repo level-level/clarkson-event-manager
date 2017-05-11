038e1d320bf975eb543fb1e7690bf8c589903fb8
Niels de Blaauw <niels@level-level.com>
2017-05-11 14:16:18 +0200

    Setup checks for dependencies

--- src/Setup.php
+++ src/Setup.php
@@ -8,14 +8,28 @@ class Setup{
     * Bootstrap the setup for the EventManager
     */
   public function __construct(){
-
+    if($this->checkDependencies()){
+      
+    }
   }
 
   /**
     * Check for Clarkson and ACF
     */
-  protected function checkDependencies(){
-
+  protected function checkDependencies() : bool{
+    if(!class_exists('Clarkson_Core')){
+      add_action( 'admin_notices', function(){
+      	echo '<div class="notice notice-error"><p>Please install Clarkson-core to use the event-manager library</p></div>';
+      });
+      return false;
+    }
+    if(!function_exists('get_fields')){
+      add_action( 'admin_notices', function(){
+      	echo '<div class="notice notice-error"><p>Please install ACF to use the event-manager library</p></div>';
+      });
+      return false;
+    }
+    return true;
   }
 
   /**
